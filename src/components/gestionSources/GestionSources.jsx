66226623987cd5c80d121cc8acd01bc29efb8fef
iheb-haddad <hadediheb9@gmail.com>
2025-01-24 01:25:10 +0100

    updates

--- src/components/gestionSources/GestionSources.jsx
+++ src/components/gestionSources/GestionSources.jsx
@@ -218,7 +218,7 @@ function GestionSources() {
 
   useEffect(() => {
     if (!webApplicationForm.projet && subProjects.length === 1) {
-      setFormData((prevData) => ({
+      setWebApplicationForm((prevData) => ({
         ...prevData,
         projet: subProjects[0]._id,
       }));
